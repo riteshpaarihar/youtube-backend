64c44a34ba1471d14a7f5db8c208526d7078478c
riteshpaarihar <ritesh.rs199@gmail.com>
2024-07-20 08:58:07 +0530

    some changes

--- src/db/index.js
+++ src/db/index.js
@@ -1,18 +1,17 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-
-
 const connectDB = async() => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URL}/${DB_NAME}`
+        );
         console.log(`Connected to ${DB_NAME} database.`);
-        console.log(`mongoDB connect ${connectionInstance.connection.host} `)
+        console.log(`mongoDB connect ${connectionInstance.connection.host} `);
     } catch (error) {
         console.error(`Error connecting to ${DB_NAME} database: ${error.message}`);
         process.exit(1);
     }
-}
-
+};
 
 export default connectDB;
---
+++ src/utils/ApiError.js
@@ -0,0 +1,25 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "Invalid",
+        errors = [],
+        stack = "") {
+        super(message);
+        this.statusCode = statusCode;
+        this.data = null;
+        this.message = message;
+        this.errors = errors;
+        this.success = false;
+
+
+        if (stack) {
+
+            this.stack = stack
+        } else {
+
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+
+export { ApiError };
